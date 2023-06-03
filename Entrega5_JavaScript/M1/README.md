f847b20add815324414a2d6e600c7371458c25bc
Franco <francolens2002@gmail.com>
2023-06-03 17:20:33 -0300

    Sumando el m1/e2 donde debiamos dividir el resultado para dar un presupuesto basico de 50% 30% y 20% tuve problemas modificando el event listener para hacerlo mas simple y entenderlo mejor pero tuve que dejarlo como en el ejemplo de fede. Inclui el segundo ejercicio en el mismo html que el primero ya que ambos tenian tematica de pyme y no especificaban lo contrario

---
+++ Entrega5_JavaScript/M1/README.md
@@ -0,0 +1,7 @@
+# Ejercicio 1 y 2 del M1 Javascript
+
+Tuvimos que recibir 3 valores para hacer una operacion matematica y devolver el resultado, similar al segundo ejercicio donde con un dato teniamos que devolver 3 valores.
+
+------------
+
+- Decidi darle personalizacion y usar html y css junto a bootstrap para practicar e implementar lo visto en clase, inclui ambos ejercicios en el mismo html  y le importe la fuente roboto
--- Entrega5_JavaScript/M1/js/ejercicio1.js
+++ Entrega5_JavaScript/M1/js/ejercicio1.js
@@ -32,3 +32,29 @@ form.addEventListener('submit', function(e){
 
 
 })
+var form2 = document.getElementById('calculadora');
+form2.addEventListener('submit', function(e){
+    e.stopPropagation;
+    var calc = e.target;
+
+    if(form2.checkValidity()){
+        var data2 = {}
+        
+        for(var i = 0; i<form2.length; i++){
+            var field2 = form2[i];
+            if(field2.getAttribute("submitable") == "true"){
+                data2 = field2.value;
+
+            }
+        }
+        var presupuesto50 = data2 * 0.5;
+        var presupuesto30 = data2 * 0.3;
+        var presupuesto20 = data2 * 0.2;
+        alert("El 50% de tu presupuesto para gastos necesarios es: " + presupuesto50)
+        alert("El 30% de tu presupuesto para gastos opcionales es: " + presupuesto30)
+        alert("Y el 20% para ahorro e inversion es: " + presupuesto20)
+    }
+
+
+
+})
