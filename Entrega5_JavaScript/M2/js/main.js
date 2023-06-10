639ed48da2ddf56b44889851cadaf91927a0fd1b
Franco <francolens2002@gmail.com>
2023-06-10 15:58:35 -0300

    Sumando m2/ej1 y ej2 validar edad y usuario con contraseña especifica, tuve problemas entendiendo como objeter el value de los inputs pero logre comprenderlo, no quede satisfecho con el diseño dle login estuve falto de inspiracion

---
+++ Entrega5_JavaScript/M2/js/main.js
@@ -0,0 +1,31 @@
+/*Edad validacion*/
+var edad= prompt("Ingrese su edad: ");
+
+if(edad >= 18){
+    alert("Bienvenido")
+} else{
+    alert("¡Debes ser mayor de edad para acceder!")
+    
+}
+
+
+/*Segundo ejercicio: Login usuario pasword*/
+
+
+var frm = document.getElementById('login')
+console.log(frm)
+frm = addEventListener('submit', event => {
+    event.stopImmediatePropagation
+    event.preventDefault
+    let forml = event.target
+    console.log(frm)
+    console.log(forml[1].value)
+    if(forml[0].value == "admin" || forml[1].value == "1234"){
+        alert("Bienvenido admin!");
+    } else{
+        alert("usuario o contraseña incorrecta pruebe nuevamente.")
+    }
+})
+
+
+
